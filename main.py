45897f6ee0b63370646dde257c495b4957d8a1f3
Alvin Irby H <thenagred@gmail.com>
2019-05-29 12:53:26 +0300

    Not throw error if user jump not to directory

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 def main():
     import os
-    import inspect
+    #import inspect
     import time
 
     t = 0
     while True:
         template_path = os.path.dirname(os.path.abspath(__file__))
-        print('\n ' * 100)
+        #print('\n ' * 100)
         print('_______________________________')
         print('[YOU AT ' + template_path + ']')
-        print(t)
         current_directory = os.listdir()
+        #for i in current_directory:
+            # Split the extension from the path and normalise it to lowercase.
+            #ext = os.path.splitext(fp)[-1].lower()
         for i in range(len(current_directory)):
             print(str(i + 1) + ". " + current_directory[i])
         if t >= 1:
@@ -19,8 +21,13 @@ def main():
                 os.chdir(previous_directory)
             elif selected_directory in current_directory:
                 previous_directory = template_path
-                os.chdir(selected_directory)
-                t += 1
+                directory_exists = os.path.isdir(selected_directory)
+                print(directory_exists)
+                if directory_exists == True:
+                    os.chdir(selected_directory)
+                else:
+                    print('It\'s not a directory. Please, select directory')
+                    time.sleep(2)
             else:
                 print('Please, select folder or press [CTRL + C] to exit')
                 time.sleep(2)
@@ -29,10 +36,20 @@ def main():
             selected_directory = input('Select directory [name] > ')
             if selected_directory in current_directory:
                 previous_directory = template_path
-                t += 1
-                os.chdir(selected_directory)
+                directory_exists = os.path.isdir(selected_directory)
+                print(directory_exists)
+                if directory_exists == True:
+                    os.chdir(selected_directory)
+                    t += 1
             else:
                 print('Please, select folder or press [CTRL + C] to exit')
                 time.sleep(2)
-                True 
+                True
+
+#import time 
+#import os
+#directory_exists = os.path.exists()
+#print(kek)
+#time.sleep(2)
+
 main()
