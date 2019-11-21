62d663787f061b3bf9f26c77171cc52f416a73ce
yuri <yuri@lovesociety.localdomain>
2019-11-21 22:16:18 +0300

    Fully working returning

--- main.py
+++ main.py
@@ -7,29 +7,20 @@ def directory_output(content):
 
 
 def previous_directory_returning(dirs_list):
-    os.chdir(dirs_list[len(dirs_list) - 2])
+    os.chdir(dirs_list[len(dirs_list) - 1])
 
 
 def removing_directory_when_go_back(dirs_list, directory):
     for i in range(len(dirs_list)):
+        print(dirs_list)
+        print(i)
         if directory == dirs_list[i]:
             dirs_list.remove(dirs_list[i])
     return dirs_list
 
 
-"""
-def removing_repeated_directories_from_history(history):
-    history_list = []
-    for i in range(len(history)):
-        history_list.append(history[i])
-    for i in range(len(history)):
-        for k in range(len(history_list)):
-            if history[i] == history_list[k]:
-                history_list.remove(history_list[k])
-    return history_list
-"""
-
 def main():
+
     os.chdir('/')
     directories_history = []
     start_directory = os.getcwd()
@@ -38,8 +29,10 @@ def main():
     directory_output(start_directory_content)
 
     while True:
+
         print('directories history' + str(directories_history))
         user_action = input('Back or forward?[b/f]')
+
         if user_action == 'f':
             selecting_directory = input('Enter directory name to jump to it\n>>>')
             os.chdir(selecting_directory)
