1d741076c43d91b3dc270ea90552f012fe00f358
yuri <yuri@lovesociety.localdomain>
2019-11-29 19:47:01 +0300

    requesting folder size

--- main.py
+++ main.py
@@ -9,17 +9,50 @@ def get_folder_elements(element, list):
         element_path = os.path.abspath(element)
         list.append(element_path)
     elif os.path.isdir(element):
+        list.append(element)
         folder = os.listdir(element)
         for i in range(len(folder)):
             temp_path = os.path.abspath(element)
-            #print(temp_path)
             element_path = os.path.abspath(os.path.join(temp_path, folder[i]))
             if os.path.isfile(element_path):
                 list.append(element_path)
             elif os.path.isdir(element_path):
+                list.append(element_path)
                 temp_path = os.path.join(element_path)
                 get_folder_elements(temp_path, list)
     return list
+
+
+def get_folder_size(folder):
+
+
+    folder_size = os.path.getsize(folder)
+    subfolders_list = get_folder_elements(folder, [])
+    for i in range(len(subfolders_list)):
+        folder_size += os.path.getsize(subfolders_list[i])
+
+    if folder_size < 1024:
+        folder_size = str(folder_size) + ' b'
+    elif folder_size in range(1024, 1048576):
+        folder_size = folder_size / 1024
+        folder_size = float('%.2f' % folder_size)
+        folder_size = str(folder_size) + ' kb'
+    elif folder_size in range(1048576, 1073741824):
+        folder_size = folder_size / 1024**2
+        folder_size = float('%.2f' % folder_size)
+        folder_size = str(folder_size) + ' mb'
+    elif folder_size in range(1073741824, 1099511627776):
+        folder_size = folder_size / 1024**3
+        folder_size = float('%.2f' % folder_size)
+        folder_size = str(folder_size) + ' gb'
+    elif folder_size >= 1099511627776:
+        folder_size = folder_size / 1024**4
+        folder_size = float('%.2f' % folder_size)
+        folder_size = str(folder_size) + ' tb'
+
+    return folder_size
+    folder_size = 0
+    subfolders_list = []
 
 
 def directory_output(content):
@@ -40,40 +73,35 @@ def removing_directory_when_go_back(dirs_list, directory):
 
 def main():
 
-    folder_elements = []
+
     os.chdir('/home/yuri/Python')
     directories_history = []
     start_directory = os.getcwd()
     directories_history.append(start_directory)
     start_directory_content = os.listdir(start_directory)
-    for i in range(len(start_directory_content)):
-        folder_elements = get_folder_elements(start_directory_content[i], folder_elements)
     directory_output(start_directory_content)
-    folder_elements = []
 
     while True:
 
-        user_action = input('Back or forward?[b/f]')
+        user_action = input('Back or forward? [b/f]\n>>>')
 
         if user_action == 'f':
             selecting_directory = input('Enter directory name to jump to it\n>>>')
+            if input('Request ' + selecting_directory + ' size? [y/n]\n>>>') == 'y':
+                size = get_folder_size(selecting_directory)
+                print(size)
             os.chdir(selecting_directory)
             current_directory = os.getcwd()
             directories_history.append(current_directory)
             current_directory_content = os.listdir(current_directory)
-            for i in range(len(current_directory_content)):
-                folder_elements = get_folder_elements(current_directory_content[i], folder_elements)
             directory_output(current_directory_content)
-            folder_elements = []
+
         elif user_action == 'b':
             directories_history = removing_directory_when_go_back(directories_history, current_directory)
             previous_directory_returning(directories_history)
             current_directory = os.getcwd()
             current_directory_content = os.listdir(current_directory)
-            for i in range(len(current_directory_content)):
-                folder_elements = get_folder_elements(current_directory_content[i], folder_elements)
             directory_output(current_directory_content)
-            folder_elements = []
 
 
 if __name__ == '__main__':
